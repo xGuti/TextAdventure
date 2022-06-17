a3cb4998695d364ab3be9dc9c96fab8e0d023607
Karol Hartwig <43039961+xGuti@users.noreply.github.com>
2022-06-17 20:28:58 +0200

    Create settings
    Changed .txt to app.config file, create menu and settings system.

--- Svends_tale/Svends_tale/Program.cs
+++ Svends_tale/Svends_tale/Program.cs
@@ -1,33 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace Svends_tale
 {
     class Program
     {
+        public static string homePath = "C:\\Users\\Guti\\Documents\\GitHub\\TextAdventure\\Svends_tale\\Svends_tale";
+
+        public static UserSettings us;
+        static List<string> activeCommands;
+        static Menu menu;
+
         static void Main(string[] args)
         {
-            int picked_number = 0;
-            Menu menu = new Menu();
-            do{
+            us = new UserSettings();
+
+            Load_User_Settings();
+            //var value = System.Configuration.ConfigurationManager.AppSettings["a"];
+            //Console.WriteLine(value);
+
+            activeCommands = new List<string>();
+
+            string command = "";
+            menu = new Menu();
+
+            do
+            {
+                //main menu
                 menu.Run();
 
-                picked_number = Convert.ToInt32(Console.ReadKey().KeyChar - 48);
-                System.Threading.Thread.Sleep(1000);
-                Console.Clear();
-
-                switch (picked_number)
+                command = Read_Command($"menu-commands-{us.Language}");
+                //CHANGE SWITCH TO SWITCH8.0    powered by Oskar Szymański
+                switch (command.ToLower())
                 {
-                    case 1:
+                    case string c when c.Equals(activeCommands[0]):
                         break;
 
-                    case 2:
+                    case string c when c.Equals(activeCommands[1]):
                         break;
 
-                    case 3:
-                        menu.Show_Commands();
+                        //commands window
+                    case string c when c.Equals(activeCommands[2]):
+                        do
+                        {
+                            menu.Show_Commands();
+                            command = Read_Command($"commands-commands-{us.Language}");
+                        } while (command != activeCommands[0]);
                         break;
 
-                    case 4:
+                        //settings window
+                    case string c when c.Equals(activeCommands[3]):
+                        do
+                        {
+                            menu.Show_Settings();
+                            command = Read_Command($"settings-commands-{us.Language}");
+
+                            switch (command)
+                            {
+                                case string c2 when c2.Equals(activeCommands[0]):
+                                    Change_Setting(typeof(ConsoleColor), "ConsoleBackgroundColor");
+                                    break;
+
+                                case string c2 when c2.Equals(activeCommands[1]):
+                                    Change_Setting(typeof(ConsoleColor), "ConsoleForegroundColor");
+                                    break;
+
+                                case string c2 when c2.Equals(activeCommands[2]):
+                                    Change_Setting(typeof(string), "Language");
+                                    break;
+
+                                case string c2 when c2.Equals(activeCommands[3]):
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Brak opcji. . .");
+                                    break;
+                            }
+
+                        } while (command != activeCommands[3]);
+
+                        break;
+
+                    case string c when c.Equals(activeCommands[4]):
+                        command = "";
                         break;
 
                     default:
@@ -35,7 +94,82 @@ namespace Svends_tale
                         System.Threading.Thread.Sleep(5000);
                         break;
                 }
-            } while (picked_number != 4);
+            } while (command != "");
+
+        }
+
+        public static void Set_Active_Commands(string usable)
+        {
+            activeCommands.Clear();
+            //only english works
+            //TODO: ADD -pl TO APP.CONFIG
+            try
+            {
+                var value = System.Configuration.ConfigurationManager.AppSettings[usable];
+                foreach (string v in value.Split(" "))
+                    activeCommands.Add(v.ToLower());
+            }
+            catch(Exception e)
+            {
+                us.Language = "en";
+                us.Save();
+            }
+        }
+
+        public static string Read_Command(string usable)
+        {
+
+            Set_Active_Commands(usable);
+
+            Console.Write(">");
+            string userCommand = Console.ReadLine();
+
+            if(activeCommands.Contains(userCommand))
+                return userCommand;
+
+            else return "error";
+        }
+
+        public static void Change_Setting(Type type, string name)
+        {
+            string command;
+            if (type == typeof(ConsoleColor))
+            {
+                menu.Show_Colors();
+                command = Read_Command($"colors-commands-{us.Language}");
+            }
+            else
+            {
+                menu.Show_Languages();
+                command = Read_Command($"languages-commands");
+            }
+                dynamic value = command switch{
+                    "black" => ConsoleColor.Black,
+                    "white" => ConsoleColor.White,
+                    "blue" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkBlue : ConsoleColor.Blue,
+                    "green" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkGreen : ConsoleColor.Green,
+                    "cyan" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkCyan : ConsoleColor.Cyan,
+                    "red" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkRed : ConsoleColor.Red,
+                    "magenta" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkMagenta : ConsoleColor.Magenta,
+                    "yellow" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkYellow : ConsoleColor.Yellow,
+                    "gray" => name == "ConsoleBackgroundColor" ? ConsoleColor.DarkGray : ConsoleColor.Gray,
+                    "english" => "\"en\"",
+                    "polski" => "\"pl\"",
+                    _ => ""
+                };
+
+            us.ConsoleBackgroundColor = name == "ConsoleBackgroundColor" ? value : us.ConsoleBackgroundColor;
+            us.ConsoleForegroundColor = name == "ConsoleForegroundColor" ? value : us.ConsoleForegroundColor;
+            us.Language = name == "Language" ? value : us.Language;
+
+            us.Save();
+            Load_User_Settings();
+        }
+
+        static void Load_User_Settings()
+        {
+            Console.BackgroundColor = us.ConsoleBackgroundColor;
+            Console.ForegroundColor = us.ConsoleForegroundColor;
         }
     }
 }
---
+++ Svends_tale/Svends_tale/UserSettings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UserSettings : ApplicationSettingsBase
+{
+    [UserScopedSetting]
+    [DefaultSettingValue("black")]
+    public ConsoleColor ConsoleBackgroundColor
+    {
+        get
+        {
+            return ((ConsoleColor)this["ConsoleBackgroundColor"]);
+        }
+        set
+        {
+            this["ConsoleBackgroundColor"] = (ConsoleColor)value;
+        }
+    }
+
+    [UserScopedSetting]
+    [DefaultSettingValue("white")]
+    public ConsoleColor ConsoleForegroundColor
+    {
+        get
+        {
+            return ((ConsoleColor)this["ConsoleForegroundColor"]);
+        }
+        set
+        {
+            this["ConsoleForegroundColor"] = (ConsoleColor)value;
+        }
+    }
+
+    [UserScopedSetting]
+    [DefaultSettingValue("en")]
+    public String Language
+    {
+        get
+        {
+            return (String)this["Language"];
+        }
+        set
+        {
+            this["Language"] = (String)value;
+        }
+    }
+}
+
