95407e76c3f819afc5efde832e7ff218c7195606
Karol Hartwig <43039961+xGuti@users.noreply.github.com>
2022-06-22 22:03:31 +0200

    Code review and doc
    Added code documentation. Cleared code in Menu. Changed class name Menu->GameWindow

---
+++ Svends_tale/Svends_tale/GameWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svends_tale
+{
+    
+    class GameWindow
+    {        
+        public void Show_Menu(string menuName)
+        {
+            Write_Header();
+
+            var lines = System.Configuration.ConfigurationManager.AppSettings[$"{menuName}-{Program.us.Language}"];
+
+            Console.WriteLine("\t\t" + lines);
+        }
+
+        public void Write_Header()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                " _______           _______  _        ______   _  _______   _________ _______  _        _______     \n" +
+                "(  ____ \\|\\     /|(  ____ \\( (    /|(  __  \\ ( )(  ____ \\  \\__   __/(  ___  )( \\      (  ____ \\    \n" +
+                "| (    \\/| )   ( || (    \\/|  \\  ( || (  \\  )|/ | (    \\/     ) (   | (   ) || (      | (    \\/    \n" +
+                "| (_____ | |   | || (__    |   \\ | || |   ) |   | (_____      | |   | (___) || |      | (__        \n" +
+                "(_____  )( (   ) )|  __)   | (\\ \\) || |   | |   (_____  )     | |   |  ___  || |      |  __)       \n" +
+                "      ) | \\ \\_/ / | (      | | \\   || |   ) |         ) |     | |   | (   ) || |      | (          \n" +
+                "/\\____) |  \\   /  | (____/\\| )  \\  || (__/  )   /\\____) |     | |   | )   ( || (____/\\| (____/\\    \n" +
+                "\\_______)   \\_/   (_______/|/    )_)(______/    \\_______)     )_(   |/     \\|(_______/(_______/    \n" +
+
+                 "\n\n");
+        }
+    }
+}
